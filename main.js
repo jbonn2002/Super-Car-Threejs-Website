973fc320dd842900f489c8eb29e9e67f422109f3
Jordy <jbonn2002@gmail.com>
2023-03-15 20:26:55 -0700

    started coding in new environment, added 3d object with envmap, and added orbit controls

--- main.js
+++ main.js
@@ -1,23 +1,49 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from 'three';
+import {RGBELoader} from 'three/examples/jsm/loaders/RGBELoader.js';
+import { FloatType } from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const scene = new THREE.Scene();
+scene.background = new THREE.Color("#CCDDD3");
 
-setupCounter(document.querySelector('#counter'))
+const camera = new THREE.PerspectiveCamera( 45, innerWidth / innerHeight, 0.1, 1000);
+// camera.position.set( -17,31,33, );
+camera.position.set(0,0,50)
+
+
+const renderer = new THREE.WebGLRenderer( { antialias: true} );
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.outputEncoding = THREE.sRGBEncoding;
+renderer.useLegacyLights = true;
+document.body.appendChild( renderer.domElement )
+
+const controls = new OrbitControls( camera, renderer.domElement );
+controls.target.set( 0, 0, 0 )
+controls.dampingFactor = 0.05;
+controls.enableDamping = true;
+
+let envmap;
+
+
+(async function(){
+  let pmrem = new THREE.PMREMGenerator(renderer);
+  let envmapTexture = await new RGBELoader().setDataType(FloatType).loadAsync("/clarens-night.hdr");
+  envmap = pmrem.fromEquirectangular(envmapTexture).texture;
+  
+  let spehreMesh = new THREE.Mesh(
+    new THREE.SphereGeometry( 5, 10, 10 ),
+    new THREE.MeshStandardMaterial({ 
+      envMap: envmap,
+      roughness: 0,
+      metalness: 1
+    })
+  );
+  scene.add(spehreMesh);
+
+  renderer.setAnimationLoop(() => {
+    renderer.render(scene,camera)
+  });
+})();
+
